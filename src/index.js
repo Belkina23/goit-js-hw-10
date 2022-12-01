b06fc78daa8e62d6df3165cfeb4cc6689c3e52ba
Belkina23 <23zakat@gmail.com>
2022-12-01 12:06:32 +0200

    efds

--- src/index.js
+++ src/index.js
@@ -15,60 +15,61 @@ const refs = {
 refs.searchBox.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY))
 
 function inputCountry() {
-  const name = refs.searchBox.value.trim()
-  if (name === '') {
-    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
-  }
+    const name = refs.searchBox.value.trim()
+    if (name === '') {
+        return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
+    }
 
-  fetchCountries(name)
-    .then(countries => {
-      refs.countryList.innerHTML = ''
-      refs.countryInfo.innerHTML = ''
-      if (countries.length === 1) {
-        refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
-        refs.countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
-      } else if (countries.length >= 10) {
-        alertTooManyMatches()
-      } else {
-        refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
-      }
-    })
-    .catch(alertWrongName)
+    fetchCountries(name)
+        .then(countries => {
+            refs.countryList.innerHTML = ''
+            refs.countryInfo.innerHTML = ''
+            if (countries.length === 1) {
+                refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+                refs.countryInfo.insertAdjacentHTML('beforeend', renderCountryInfo(countries))
+            } else if (countries.length >= 10) {
+                alertTooManyMatches()
+            } else {
+                refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries))
+            }
+        })
+        .catch(alertWrongName)
 }
 
+function alertWrongName() {
+    Notiflix.Notify.failure('Oops, there is no country with that name')
+}
+
+function alertTooManyMatches() {
+    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+}
+
+
 function renderCountryList(countries) {
-  const markup = countries
-    .map(({ name, flags }) => {
-      return `
+    const markup = countries
+        .map(({ name, flags }) => {
+            return `
           <li class="country-list__item">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}">
               <h2 class="country-list__name">${name.official}</h2>
           </li>
           `
-    })
-    .join('')
-  return markup
+        })
+        .join('')
+    return markup
 }
 
 function renderCountryInfo(countries) {
-  const markup = countries
-    .map(({ capital, population, languages }) => {
-      return `
+    const markup = countries
+        .map(({ capital, population, languages }) => {
+            return `
         <ul class="country-info__list">
             <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
             <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
             <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
         </ul>
         `
-    })
-    .join('')
-  return markup
-}
-
-function alertWrongName() {
-  Notiflix.Notify.failure('Oops, there is no country with that name')
-}
-
-function alertTooManyMatches() {
-  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+        })
+        .join('')
+    return markup
 }
