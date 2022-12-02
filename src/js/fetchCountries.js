9c27648651cf90d2645aee06aaa7423a60649d30
Belkina23 <23zakat@gmail.com>
2022-12-02 21:29:19 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,6 +4,10 @@ const fields = 'fields=name,capital,population,flags,languages'
 
 export function fetchCountries(name) {
   return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.json())
-    .catch(error => console.log(error))
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
